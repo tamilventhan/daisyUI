973b60a5fd4795f9bf7b57e23bb51afb52bb6236
Tamilventhan T <78096762+tamilventhan@users.noreply.github.com>
2024-04-10 18:44:19 +0530

    added vendor component

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link href="{{ route('vendors') }}" :active="request()->routeIs('vendors')">
+                        {{ __('Vendors') }}
+                    </x-nav-link>
                 </div>
             </div>
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\Vendors;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,4 +15,6 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::get('/vendors',Vendors::class)->name('vendors');
 });
